0cd77a236c6e4ca8ce2f2679ce940aad2e092d83
Matheus Barretto <78603546+mattd-silva22@users.noreply.github.com>
2022-07-30 17:06:44 -0300

    fix order query

--- src/api/repository/OrderRepository.ts
+++ src/api/repository/OrderRepository.ts
@@ -1,5 +1,6 @@
 import { connection } from "../../connection"
 import { Category } from "../models/Category"
+import { NewOrder, Order } from "../models/Order"
 
 export class CategoriesRepository {
 
@@ -15,24 +16,16 @@ export class CategoriesRepository {
         return queryResult[0]
     }
 
-    async create(categoryData:Category) {
-        const category = [categoryData.name]
-        const query = " INSERT INTO category(name) VALUE (?) "
-        const queryResult = await connection.promise().query(query, category)
+    async create(categoryData:NewOrder) {
+        const category = []
+        const query = ` INSERT  INTO orders  (id_client,id_items,create_at)  VALUE (?,?,?,?);
+                        INSERT INTO itens_oders (id_order,id_product,amount) VALUE (LAST_INSERT_ID(),?,?);
+        
+        `
+        const queryResult = await connection.promise().query(query, [])
         return queryResult
     }
     // UPDATE Customers SET ContactName = 'Alfred Schmidt', City = 'Frankfurt' WHERE CustomerID = 1;
 
-    async update(categoryData:Category) {
-        const category = [categoryData.name , categoryData.id]
-        const query = " UPDATE category SET name = ? WHERE id = ?"
-        const queryResult = await connection.promise().query(query, category)
-        return queryResult
-    }
-
-    async delete(id:string) {
-        const query = "DELETE FROM category WHERE id = ?"
-        const queryResult = await connection.promise().query(query, [id])
-        return queryResult
-    }
+   
 }
